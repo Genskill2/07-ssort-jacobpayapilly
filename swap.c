0d35302b9a10f7d210e345ea27230bd0e52e191e
jacobpayapilly <85183281+jacobpayapilly@users.noreply.github.com>
2021-06-29 20:13:16 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,7 +1,6 @@
 void swap_max(int arr[],int l,int n){
 int m=arr[n];
-int p;
-p=n;
+int p=n;
 for (int i=n;i<l;i++){
 if(arr[i]>m)
 {m=arr[i];
@@ -10,10 +9,11 @@ p=i;}
 int t;
 t=arr[p];
 arr[p]=arr[n];
-arr[n]=arr[p];
+arr[n]=t;
 }
 
 void ssort(int arr[],int l)
-{for(int i=0;i<n-1;i++)
+{for(int i=0;i<l-1;i++)
 {swap_max(arr[],l,i);
+}
 }
